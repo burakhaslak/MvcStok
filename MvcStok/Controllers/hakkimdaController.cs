726e954389f18aa89820079324ee68cc15af292b
burakhaslak <burakhaslak@DESKTOP-IV25BQJ>
2025-11-29 15:48:06 +0300

    Add project files.

---
+++ MvcStok/Controllers/hakkimdaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcStok.Controllers
+{
+    public class hakkimdaController : Controller
+    {
+        // GET: hakkimda
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
